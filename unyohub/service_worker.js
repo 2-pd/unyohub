65522c600cde1767c94748bb755e7fa14c8d0d93
kazuma <>
2023-10-08 17:30:49 +0000

    フロントエンド開発開始、スプラッシュスクリーンと設定画面を設置
    FossilOrigin-Name: 6731166fa3bcf2aa8f379ccb35402b62074d8a42b49a6a56fc4e35251c4a575b

---
+++ unyohub/service_worker.js
@@ -0,0 +1,43 @@
+var unyo_version = "23.10-1"; /*運用観察Hub本体のバージョン番号*/
+
+var cache_name = "unyohub_" + unyo_version;
+var files_to_cache = [
+        "./",
+        "apple-touch-icon.webp",
+        "maskable_icon.webp",
+        "favicon.ico",
+        "splash_screen_image.webp",
+        "README.html"
+    ];
+
+self.addEventListener("install", function(event) {
+    event.waitUntil(
+        caches.open(cache_name).then(function(cache) {
+            return cache.addAll(files_to_cache);
+        })
+    );
+});
+
+self.addEventListener("fetch", function(event) {
+    event.respondWith(
+        caches.match(event.request).then(function(response) {
+            if (response) {
+                return response;
+            }
+            
+            return fetch(event.request);
+        })
+    );
+});
+
+self.addEventListener("activate", function(event) {
+    event.waitUntil(
+        caches.keys().then(function(cacheNames) {
+            return Promise.all(cacheNames.map(function(cacheName) {
+                    if (cacheName != cache_name){
+                        return caches.delete(cacheName);
+                    }
+            }));
+        })
+    );
+});
