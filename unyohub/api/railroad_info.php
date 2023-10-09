4841f2ef9bd2cb67437bf4e0ef866d6345391c30
kazuma <>
2023-10-09 17:16:33 +0000

    路線情報の取得処理とキャッシュ化処理を実装
    FossilOrigin-Name: 40c7e53cdf97e17cf8db75661d0d13bddf3a50ca018a2821ee94e7e4d8d61632

---
+++ unyohub/api/railroad_info.php
@@ -0,0 +1,21 @@
+<?php
+if (!isset($_POST["railroad_id"], $_POST["last_modified_timestamp"])) {
+    print "ERROR: 送信値が不正です";
+    exit;
+}
+
+$path = "../data/".basename($_POST["railroad_id"])."/railroad_info.json";
+
+if (!file_exists($path)) {
+    print "ERROR: 路線系統データがありません";
+    exit;
+}
+
+$last_modified = @filemtime($path);
+
+if (intval($_POST["last_modified_timestamp"]) >= $last_modified) {
+    print "NO_UPDATES_AVAILABLE";
+} else {
+    header("Last-Modified: ".gmdate("D, d M Y H:i:s", $last_modified)." GMT");
+    readfile($path);
+}
