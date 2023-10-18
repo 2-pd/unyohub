211d3cedc83e6a88712a4945eb59312ac315de1d
kazuma <>
2023-10-18 21:41:18 +0000

    走行位置表示での充当編成の表示に対応
    FossilOrigin-Name: 935b81d2cc61c29c79825fb59b9986cc00c0dc0e455b384951e55c34fa7c2200

---
+++ unyohub/api/operation_data.php
@@ -0,0 +1,32 @@
+<?php
+if (!isset($_POST["railroad_id"], $_POST["date"], $_POST["last_modified_timestamp"])) {
+    print "ERROR: 送信値が不正です";
+    exit;
+}
+
+$db_obj = new SQLite3("../data/".basename($_POST["railroad_id"])."/railroad.db");
+$db_obj->busyTimeout(5000);
+
+$operations_r = $db_obj->query("SELECT `operation_number`, `formations`, `variants_count`, `updated_datetime` FROM `unyohub_data_caches` WHERE `operation_date` = '".$db_obj->escapeString($_POST["date"])."' AND `updated_datetime` > '".date("Y-m-d H:i:s", intval($_POST["last_modified_timestamp"]))."' ORDER BY `updated_datetime` DESC");
+
+$operation_data = array();
+while ($operation = $operations_r->fetchArray(SQLITE3_ASSOC)) {
+    if (empty($operation_data)) {
+        $updated_datetime = $operation["updated_datetime"];
+    }
+    $operation_data[$operation["operation_number"]] = array("formations" => $operation["formations"], "variants_count" => $operation["variants_count"]);
+}
+
+if (empty($operation_data)) {
+    print "NO_UPDATES_AVAILABLE";
+} else {
+    header("Last-Modified: ".gmdate("D, d M Y H:i:s", strtotime($updated_datetime))." GMT");
+    
+    if (!empty($_SERVER["HTTP_ACCEPT_ENCODING"]) && strpos($_SERVER["HTTP_ACCEPT_ENCODING"], "gzip") !== FALSE) {
+        header("Content-Encoding: gzip");
+        
+        print gzencode(json_encode($operation_data, JSON_UNESCAPED_UNICODE));
+    } else {
+        print json_encode($operation_data, JSON_UNESCAPED_UNICODE);
+    }
+}
