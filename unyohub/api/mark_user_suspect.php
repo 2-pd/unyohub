ef2245eec6e63d0b30adde17985fe780eb9806c1
kazuma <>
2024-04-08 08:57:51 +0000

    APIエンドポイント「mark_user_suspect.php」を実装
    FossilOrigin-Name: c9c08e72e2458db84bd3d50e5ab4d9434b380b59998595d038572bfb464500ab

---
+++ unyohub/api/mark_user_suspect.php
@@ -0,0 +1,32 @@
+<?php
+include "../libs/wakarana/main.php";
+include "__operation_data_functions.php";
+
+if (!isset($_POST["user_id"])) {
+    print "ERROR: 送信値が不正です";
+    exit;
+}
+
+$wakarana = new wakarana("../config");
+
+$user = $wakarana->check();
+if (is_object($user)) {
+    if ($user->check_one_time_token($_POST["one_time_token"])) {
+        if (!$user->check_permission("moderate")) {
+            print "ERROR: モデレーター権限が確認できませんでした。モデレーター専用機能は使用できません";
+            exit;
+        }
+    } else {
+        print "ERROR: ワンタイムトークンの認証に失敗しました。再度ご送信ください";
+        exit;
+    }
+} else {
+    print "ERROR: ログイン情報の有効期限が切れています。再ログインしてください";
+    exit;
+}
+
+connect_moderation_db();
+
+$moderation_db_obj->query("INSERT INTO `unyohub_moderation_suspicious_users` (`user_id`, `moderator_id`, `marked_datetime`) VALUES ('".$moderation_db_obj->escapeString($_POST["user_id"])."', '".$user->get_id()."', '".date("Y-m-d H:i:s")."')");
+
+print "SUCCEEDED";
