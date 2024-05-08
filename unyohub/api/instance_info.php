4fde17e7a8f87f489f71dd835c1b956a03cfb506
kazuma <>
2024-05-08 17:11:00 +0000

    v24.05-2 インスタンス情報取得機能の部分実装等
    FossilOrigin-Name: 3c6b2f6c06b5e2d91a7af3673633c6b2033580ced02013cc3e493b31ebb48f73

---
+++ unyohub/api/instance_info.php
@@ -0,0 +1,23 @@
+<?php
+define("MAIN_CONFIG_PATH", "../config/main.ini");
+
+$last_modified = @filemtime(MAIN_CONFIG_PATH);
+if ($last_modified === FALSE) {
+    print "ERROR: main.iniにアクセスできません";
+    exit;
+}
+
+if (isset($_POST["last_modified_timestamp"]) && $last_modified <= intval($_POST["last_modified_timestamp"])) {
+    print "NO_UPDATES_AVAILABLE";
+} else {
+    header("Last-Modified: ".gmdate("D, d M Y H:i:s", $last_modified)." GMT");
+    
+    $main_config = parse_ini_file(MAIN_CONFIG_PATH);
+    
+    print json_encode(array(
+        "instance_name" => $main_config["instance_name"],
+        "allow_sign_up" => $main_config["allow_sign_up"],
+        "require_email_address" => $main_config["require_email_address"],
+        "allow_guest_user" => $main_config["allow_guest_user"]
+    ), JSON_UNESCAPED_UNICODE);
+}
