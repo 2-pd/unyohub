54812e0140406346f8c7a6b6fbb4afa03c5c0836
kazuma <>
2024-09-23 10:11:33 +0000

    ルールとポリシーで目次を表示、等
    FossilOrigin-Name: 55e3a140375afab40ef229179955a5549a09d0b390c699a40bc126b771c0a6d9

--- unyohub/user/rules.php
+++ unyohub/user/rules.php
@@ -16,15 +16,33 @@ if (!empty($_GET["show_accept_button"])) {
 
 print "    <h2>ルールとポリシー</h2>\n";
 
-$rules_split = explode("\n", preg_replace("/(https?:\/\/[^\s\\\\`\|\[\]\{\}\^]+)/u", "<a href='$1' target='_blank' class='external_link'>$1</a>", htmlspecialchars(file_get_contents("../rules.txt"))));
+$rules_split = explode("\n", preg_replace("/(https?:\/\/[^\s\\\\`\|\[\]\{\}\^]+)/u", "<a href='$1' target='_blank' class='external_link'>$1</a>", htmlspecialchars(file_get_contents("../config/rules.txt"))));
 
+$buf = "";
+$heading_list = array();
+$heading_cnt = 0;
 foreach ($rules_split as $rules_line) {
     if (substr($rules_line, 0, 2) === "# ") {
-        print "    <h4>".substr($rules_line, 2)."</h4>\n";
+        $heading_list[] = substr($rules_line, 2);
+        $buf .= "    <h4 id='heading_".$heading_cnt."'>".$heading_list[$heading_cnt]."</h4>\n";
+        $heading_cnt++;
     } else {
-        print "    ".$rules_line."<br>\n";
+        $buf .= "    ".$rules_line."<br>\n";
     }
 }
+
+if ($heading_cnt >= 1) {
+    print "    <h4>目次</h4>\n";
+    print "    <ul>\n";
+    
+    for ($cnt = 0; $cnt < $heading_cnt; $cnt++) {
+        print "        <li><a href='#heading_".$cnt."'>".$heading_list[$cnt]."</a></li>\n";
+    }
+    
+    print "    </ul>\n";
+}
+
+print $buf;
 
 if (!empty($_GET["show_accept_button"])) {
     print "    <button type=\"button\" class=\"wide_button\" onclick=\"window.opener.accept_rules(); window.close();\">ルールに同意して続行</button>\n";
