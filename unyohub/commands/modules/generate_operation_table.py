b240de63b2a0f6361daad45c24d81b71bc50c875
kazuma <>
2025-10-11 06:00:00 +0000

    コマンドラインツールにgenerate_operation_table.pyを実装
    FossilOrigin-Name: d1516ba7c19f670841e781d8c22704a9801f5f67e5bd75ec35d167c5f96b3562

---
+++ unyohub/commands/modules/generate_operation_table.py
@@ -0,0 +1,104 @@
+# coding: utf-8
+
+import csv
+import json
+import sqlite3
+
+from modules import diagram_funcs
+
+def generate_operation_table (mes, main_dir, date_string):
+    mes("運用表の生成", is_heading=True)
+    
+    
+    diagram = diagram_funcs.diagram(main_dir, mes=mes)
+    
+    diagram_revision, diagram_id = diagram.get_diagram_id(date_string)
+    
+    if diagram_id is None:
+        mes("指定された日付に該当するダイヤを判別できませんでした", True)
+        return False
+    
+    
+    mes("timetable_" + diagram_id + ".jsonを読み込んでいます...")
+    with open(main_dir + "/" + diagram_revision + "/timetable_" + diagram_id + ".json", "r", encoding="utf-8-sig") as json_f:
+        timetable = json.load(json_f)
+    
+    trains = {}
+    
+    for line_id in timetable.keys():
+        for direction in ["inbound_trains", "outbound_trains"]:
+            for train_number in timetable[line_id][direction].keys():
+                if train_number not in trains:
+                    trains[train_number] = { "first_departure_time" : None, "final_arrival_time" : None }
+                
+                for departure_time in timetable[line_id][direction][train_number][0]["departure_times"]:
+                    if departure_time is not None:
+                        first_departure_time = departure_time
+                        break
+                
+                for departure_time in reversed(timetable[line_id][direction][train_number][-1]["departure_times"]):
+                    if departure_time is not None:
+                        final_arrival_time = departure_time
+                        break
+                
+                if trains[train_number]["first_departure_time"] is None or first_departure_time < trains[train_number]["first_departure_time"]:
+                    trains[train_number]["first_departure_time"] = first_departure_time
+                
+                if trains[train_number]["final_arrival_time"] is None or final_arrival_time > trains[train_number]["final_arrival_time"]:
+                    trains[train_number]["final_arrival_time"] = final_arrival_time
+    
+    
+    mes("データベースに接続しています...")
+    conn = sqlite3.connect(main_dir + "/railroad.db")
+    cur = conn.cursor()
+    
+    
+    mes("在線履歴を抽出しています...")
+    cur.execute("SELECT `unyohub_operation_logs`.`vehicle_id`, `unyohub_trip_ids`.`train_number` FROM `unyohub_operation_logs`, `unyohub_trip_ids` WHERE `unyohub_operation_logs`.`operation_date` = :operation_date AND `unyohub_trip_ids`.`diagram_revision` = :diagram_revision AND `unyohub_trip_ids`.`diagram_id` = :diagram_id AND `unyohub_operation_logs`.`trip_id` = `unyohub_trip_ids`.`trip_id`", { "operation_date" : date_string, "diagram_revision" : diagram_revision, "diagram_id" : diagram_id })
+    
+    operation_logs = {}
+    starting_times = {}
+    
+    for log_data in cur.fetchall():
+        if log_data[0] not in operation_logs:
+            operation_logs[log_data[0]] = {}
+            starting_times[log_data[0]] = None
+        
+        operation_logs[log_data[0]][trains[log_data[1]]["first_departure_time"]] = log_data[1]
+        
+        if starting_times[log_data[0]] is None or trains[log_data[1]]["first_departure_time"] < starting_times[log_data[0]]:
+            starting_times[log_data[0]] = trains[log_data[1]]["first_departure_time"]
+    
+    conn.close()
+    
+    
+    mes("運用表を生成しています...")
+    
+    vehicle_ids = {}
+    
+    for vehicle_id in starting_times.keys():
+        vehicle_ids[starting_times[vehicle_id] + "__" + vehicle_id] = vehicle_id
+    
+    operation_table = [["# 全ての運用"]]
+    
+    for time_key in sorted(vehicle_ids.keys()):
+        vehicle_id = vehicle_ids[time_key]
+        
+        operation = ["", "", "", "", "", ""]
+        
+        for time_key in sorted(operation_logs[vehicle_id].keys()):
+            operation.append(operation_logs[vehicle_id][time_key])
+        
+        operation[0] = operation[6]
+        
+        operation_table.append(operation)
+    
+    
+    mes("運用表を operation_table_" + diagram_id + ".csv に書き込んでいます...")
+    
+    with open(main_dir + "/" + diagram_revision + "/operation_table_" + diagram_id + ".csv", "w", encoding="utf-8-sig") as csv_f:
+        csv_writer = csv.writer(csv_f, lineterminator="\n")
+        csv_writer.writerows(operation_table)
+    
+    
+    mes("処理が完了しました")
