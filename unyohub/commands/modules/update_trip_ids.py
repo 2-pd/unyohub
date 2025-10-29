25e046138d1d4a8555d3d852d10c2455124b9aa2
kazuma <>
2025-10-29 08:00:00 +0000

    コマンドラインツールからDBに登録済みの運用表や便識別名対応表を削除可能に
    FossilOrigin-Name: a8fd92edee49d76e4c1615259c00bcbffe7a56e40f5a66f8efbbc9b87f72360b

--- unyohub/commands/modules/update_trip_ids.py
+++ unyohub/commands/modules/update_trip_ids.py
@@ -4,7 +4,7 @@
 import csv
 import sqlite3
 
-def update_trip_ids (mes, main_dir, diagram_revision, diagram_id):
+def update_trip_ids (mes, main_dir, diagram_revision, diagram_id, options=set()):
     mes("便識別名と列車番号の対応表をデータベースに書き込み", is_heading=True)
     
     
@@ -13,23 +13,25 @@ def update_trip_ids (mes, main_dir, diagram_revision, diagram_id):
     cur = conn.cursor()
     
     
-    mes("train_number_mappings_" + diagram_id + ".csv を読み込んでいます...")
-    with open(main_dir + "/" + diagram_revision + "/train_number_mappings_" + diagram_id + ".csv", "r", encoding="utf-8-sig") as csv_f:
-        csv_reader = csv.reader(csv_f)
-        train_number_mappings = [data_row for data_row in csv_reader]
+    if "-D" not in options:
+        mes("train_number_mappings_" + diagram_id + ".csv を読み込んでいます...")
+        with open(main_dir + "/" + diagram_revision + "/train_number_mappings_" + diagram_id + ".csv", "r", encoding="utf-8-sig") as csv_f:
+            csv_reader = csv.reader(csv_f)
+            train_number_mappings = [data_row for data_row in csv_reader]
     
     
     mes("データベースから " + diagram_revision + " 改正ダイヤ " + diagram_id + " の古い便識別名データを削除します...")
     cur.execute("DELETE FROM `unyohub_trip_ids` WHERE `diagram_revision` = :diagram_revision AND `diagram_id` = :diagram_id", {"diagram_revision" : diagram_revision, "diagram_id" : diagram_id})
     
     
-    mes("便識別名データを書き込んでいます...")
-    
-    for train_number_mapping in train_number_mappings:
-        if len(train_number_mapping[0]) == 0:
-            continue
+    if "-D" not in options:
+        mes("便識別名データを書き込んでいます...")
         
-        cur.execute("INSERT INTO `unyohub_trip_ids` (`diagram_revision`, `diagram_id`, `train_number`, `trip_id`) VALUES (:diagram_revision, :diagram_id, :train_number, :trip_id)", {"diagram_revision" : diagram_revision, "diagram_id" : diagram_id, "train_number" : train_number_mapping[1], "trip_id" : train_number_mapping[0]})
+        for train_number_mapping in train_number_mappings:
+            if len(train_number_mapping[0]) == 0:
+                continue
+            
+            cur.execute("INSERT INTO `unyohub_trip_ids` (`diagram_revision`, `diagram_id`, `train_number`, `trip_id`) VALUES (:diagram_revision, :diagram_id, :train_number, :trip_id)", {"diagram_revision" : diagram_revision, "diagram_id" : diagram_id, "train_number" : train_number_mapping[1], "trip_id" : train_number_mapping[0]})
     
     
     mes("データベースの書き込み処理を完了しています...")
