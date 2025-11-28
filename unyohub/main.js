41533b0c1187f6e69a9434f52fe2fd471cceb1b1
kazuma <>
2025-11-28 14:00:00 +0000

    車歴の「塗装変更」の表記を「塗装等変更」に書き換え
    FossilOrigin-Name: 5b1c829c614a30bc0f7ca6ef2b1ea83d4f19606a26b019c95c05e9719c3adbdc

--- unyohub/main.js
+++ unyohub/main.js
@@ -4477,7 +4477,7 @@ function formation_detail (formation_name) {
                 
                 formation_operations_area_elm.innerHTML = buf;
                 
-                var event_type_ja = { construct : "新製", modify : "改修", repaint : "塗装変更", renewal : "更新", transfer : "転属", rearrange : "組換", unregister : "廃車", other : "その他" };
+                var event_type_ja = { construct : "新製", modify : "改修", repaint : "塗装等変更", renewal : "更新", transfer : "転属", rearrange : "組換", unregister : "廃車", other : "その他" };
                 
                 var buf = "";
                 for (var history of data["histories"]) {
