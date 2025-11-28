316fe65c3ed62bab3159cd4db075daae16415ed9
kazuma <>
2025-11-28 12:00:00 +0000

    番台の前に系列名を付けずDBに登録できるオプションに対応した追加の仕様変更等
    FossilOrigin-Name: 56f85ac3bb13ec0989dbcf07767d478a5dcf999460b89c53afdf8a3d1d9d9297

--- unyohub/commands/modules/convert_formation_table.py
+++ unyohub/commands/modules/convert_formation_table.py
@@ -111,10 +111,7 @@ def convert_formation_table (mes, main_dir):
         elif formation_name.startswith("# "):
             if subseries_name is not None:
                 if subseries_max_car_count >= 1:
-                    if not series_is_series_group:
-                        subseries_name = series_name + subseries_name
-                    
-                    insert_series_data(mes, cur, subseries_name, series_name, subseries_min_car_count, subseries_max_car_count, subseries_coupling_group_set)
+                    insert_series_data(mes, cur, subseries_full_name, series_name, subseries_min_car_count, subseries_max_car_count, subseries_coupling_group_set)
                 elif not unregistered_subseries:
                     mes(subseries_name + " には在籍中の編成が存在しませんが廃区分として設定されていません", True)
             
@@ -144,6 +141,7 @@ def convert_formation_table (mes, main_dir):
             json_data["series_names"].append(series_name)
             
             subseries_name = None
+            subseries_full_name = None
             coupling_group_set = set()
             min_car_count = None
             max_car_count = 0
@@ -162,14 +160,16 @@ def convert_formation_table (mes, main_dir):
         elif formation_name.startswith("## "):
             if subseries_name is not None:
                 if subseries_max_car_count >= 1:
-                    if not series_is_series_group:
-                        subseries_name = series_name + subseries_name
-                    
-                    insert_series_data(mes, cur, subseries_name, series_name, subseries_min_car_count, subseries_max_car_count, subseries_coupling_group_set)
+                    insert_series_data(mes, cur, subseries_full_name, series_name, subseries_min_car_count, subseries_max_car_count, subseries_coupling_group_set)
                 elif not unregistered_subseries:
                     mes(subseries_name + " には在籍中の編成が存在しませんが廃区分として設定されていません", True)
             
             subseries_name = formation_name[3:].strip()
+            
+            if series_is_series_group:
+                subseries_full_name = subseries_name
+            else:
+                subseries_full_name = series_name + subseries_name
             
             if subseries_name.startswith("†"):
                 subseries_name = subseries_name[1:].strip()
@@ -283,7 +283,7 @@ def convert_formation_table (mes, main_dir):
                     unavailable_value = None
                     unavailable_q = ", `unavailable` = NULL"
                 
-                cur.execute("INSERT INTO `unyohub_formations`(`formation_name`, `currently_registered`, `series_name`, `subseries_name`, `car_count`, `affiliation`, `caption`, `description`, `semifixed_formation`, `unavailable`, `inspection_information`, `overview_updated`, `updated_datetime`, `edited_user_id`) VALUES (:formation_name, :currently_registered, :series_name, :subseries_name, :car_count, '', '', '', NULL, :unavailable, '', :overview_updated, :updated_datetime, NULL) ON CONFLICT(`formation_name`) DO UPDATE SET `currently_registered` = :currently_registered_2, `series_name` = :series_name_2, `subseries_name` = :subseries_name_2, `car_count` = :car_count_2" + unavailable_q, {"formation_name" : formation_name, "currently_registered" : currently_registered, "series_name" : series_name, "subseries_name" : subseries_name, "car_count" : car_count, "unavailable" : unavailable_value, "overview_updated" : datetime_now, "updated_datetime" : datetime_now, "currently_registered_2" : currently_registered, "series_name_2" : series_name, "subseries_name_2" : subseries_name, "car_count_2" : car_count})
+                cur.execute("INSERT INTO `unyohub_formations`(`formation_name`, `currently_registered`, `series_name`, `subseries_name`, `car_count`, `affiliation`, `caption`, `description`, `semifixed_formation`, `unavailable`, `inspection_information`, `overview_updated`, `updated_datetime`, `edited_user_id`) VALUES (:formation_name, :currently_registered, :series_name, :subseries_name, :car_count, '', '', '', NULL, :unavailable, '', :overview_updated, :updated_datetime, NULL) ON CONFLICT(`formation_name`) DO UPDATE SET `currently_registered` = :currently_registered_2, `series_name` = :series_name_2, `subseries_name` = :subseries_name_2, `car_count` = :car_count_2" + unavailable_q, {"formation_name" : formation_name, "currently_registered" : currently_registered, "series_name" : series_name, "subseries_name" : subseries_full_name, "car_count" : car_count, "unavailable" : unavailable_value, "overview_updated" : datetime_now, "updated_datetime" : datetime_now, "currently_registered_2" : currently_registered, "series_name_2" : series_name, "subseries_name_2" : subseries_full_name, "car_count_2" : car_count})
                 
                 formation_list.add(formation_name)
                 
