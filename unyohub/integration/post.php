792807827433b4c7204a66f0cf7a957b2c33610d
kazuma <>
2026-02-15 08:00:00 +0000

    外部アプリ連携向けの情報投稿画面からの新規ユーザー登録を可能に
    FossilOrigin-Name: 2d36d94794c0ded1b57883bf7a8bffde9ef1318d99ae8ca56c69b813882f21f4

--- unyohub/integration/post.php
+++ unyohub/integration/post.php
@@ -10,15 +10,15 @@ print_header("運用情報の投稿");
         
     </article>
     <script>
-        function on_logged_in (user_data) {
+        function update_user_data (user_data) {
             document.getElementById("login_status").innerHTML = "<b>" + escape_html(user_data["user_name"]) + "</b>さんとしてログイン中<br><a href='javascript:void(0);' onclick='user_logout(on_guest_mode);'>ログアウト</a>";
         }
         
         function on_guest_mode () {
-            document.getElementById("login_status").innerHTML = "<b>ゲストユーザー</b>としてアクセス中<br><a href='javascript:void(0);' onclick='show_login_form(on_logged_in);'>ログイン</a>　<a href='javascript:void(0);'>新規登録</a>";
+            document.getElementById("login_status").innerHTML = "<b>ゲストユーザー</b>としてアクセス中<br><a href='javascript:void(0);' onclick='show_login_form(update_user_data);'>ログイン</a>　<a href='javascript:void(0);' onclick='open_child_page(\"/user/sign_up.php?is_child_page=yes\");'>新規登録</a>";
         }
         
-        window.addEventListener("load", function () { check_logged_in(on_logged_in, on_guest_mode, function () {  document.getElementById("login_status").innerHTML = "ログイン状態の確認に失敗しました"; }); });
+        window.addEventListener("load", function () { check_logged_in(update_user_data, on_guest_mode, function () {  document.getElementById("login_status").innerHTML = "ログイン状態の確認に失敗しました"; }); });
     </script>
 <?php
 print_footer();
