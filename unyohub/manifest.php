c42382dcea456811b2e1ce8d943b1c46831514f4
kazuma <>
2026-02-17 04:00:00 +0000

    app_manifest.jsonを追加し、manifest.phpはその内容を参照するように
    FossilOrigin-Name: 4f2490645af723fda387254336ac3ba31bce37393fb227d147c1473bacbd7707

--- unyohub/manifest.php
+++ unyohub/manifest.php
@@ -1,50 +1,29 @@
 <?php
 header("Content-Type: application/manifest+json");
 
-$config = parse_ini_file("config/main.ini");
+$app_manifest = json_decode(file_get_contents("config/app_manifest.json"), TRUE);
+$main_config = parse_ini_file("config/main.ini");
 
-$root_url = "http".(empty($_SERVER["HTTPS"]) ? "" : "s")."://".$_SERVER["HTTP_HOST"];
-$instance_name = addslashes($config["instance_name"]);
-$instance_introduction = addslashes(str_replace("\n", " ", stripcslashes($config["instance_introduction"])));
-print <<< EOM
-{
-    "id" : "{$root_url}/",
-    "name" : "{$instance_name}",
-    "short_name" : "{$instance_name}",
-    "display" : "standalone",
-    "start_url" : "/",
-    "scope" : "/",
-    "background_color" : "#222222",
-    "theme_color" : "#333333",
-    "orientation" : "natural",
-    "icons": [
-        {
-            "src": "apple-touch-icon.webp",
-            "type": "image/webp",
-            "sizes": "192x192",
-            "purpose": "any"
-        },
-        {
-            "src": "maskable_icon.webp",
-            "type": "image/webp",
-            "sizes": "192x192",
-            "purpose": "maskable"
-        },
-        {
-            "src": "monochrome_icon.webp",
-            "type": "image/webp",
-            "sizes": "192x192",
-            "purpose": "monochrome"
-        }
-    ],
-    "categories" : ["navigation", "travel"],
-    "description" : "{$instance_introduction}",
-    "screenshots" : [
-        {
-            "src": "large_image.webp",
-            "type": "image/webp",
-            "sizes": "1600x838"
-        }
-    ]
+$output_data = array(
+    "id" => "http".(empty($_SERVER["HTTPS"]) ? "" : "s")."://".$_SERVER["HTTP_HOST"]."/",
+    "name" => $app_manifest["name"],
+    "short_name" => $app_manifest["name"],
+    "display" => "standalone",
+    "start_url" => "/",
+    "scope" => "/",
+    "background_color" => "#222222",
+    "theme_color" => $app_manifest["theme_color"],
+    "orientation" => "natural",
+    "icons" => $app_manifest["icons"],
+    "categories" => array("navigation", "travel"),
+    "description" => str_replace("\n", " ", stripcslashes($main_config["instance_introduction"])),
+    "screenshots" => $app_manifest["screenshots"]
+);
+
+if (!empty($_SERVER["HTTP_ACCEPT_ENCODING"]) && str_contains($_SERVER["HTTP_ACCEPT_ENCODING"], "gzip")) {
+    header("Content-Encoding: gzip");
+    
+    print gzencode(json_encode($output_data, JSON_UNESCAPED_UNICODE));
+} else {
+    print json_encode($output_data, JSON_UNESCAPED_UNICODE);
 }
-EOM;
