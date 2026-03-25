2242a375e6d7dcc2410052ccec156af6f39618ab
kazuma <>
2026-03-25 04:00:00 +0000

    コマンドラインツールに個別のサブコマンドのヘルプ表示機能を実装
    FossilOrigin-Name: 871f57459f15ccedc6f2dbed4e4e4bf55780053bc77861dc57ef1ef70e6f555d

--- unyohub/commands/modules/help_funcs.py
+++ unyohub/commands/modules/help_funcs.py
@@ -30,3 +30,164 @@ def show_help (subcommand=""):
             print("  \x1b[93mgenerate-operation-table\x1b[0m    1日分の在線情報をもとに運用表を生成する")
             
             print("\n各サブコマンドのあとに --help を付加して実行するとそのサブコマンドの詳細説明が表示されます")
+        
+        case "setup-libs":
+            print("\x1b[93munyohub setup-libs\x1b[0m  ライブラリの初期設定を行う")
+            
+            print("\n引数")
+            print("  \x1b[33m第1引数\x1b[0m : 管理者ユーザーID(省略時は「unyohub_admin」)")
+        
+        case "embed-icons":
+            print("\x1b[93munyohub embed-icons\x1b[0m  車両アイコン画像をJSONファイルに埋め込む")
+            
+            print("\n引数")
+            print("  \x1b[33m第1引数\x1b[0m : 路線系統識別名")
+        
+        case "conv-formations":
+            print("\x1b[93munyohub conv-formations\x1b[0m  編成表データを変換する")
+            
+            print("\n引数")
+            print("  \x1b[33m第1引数\x1b[0m : 路線系統識別名")
+        
+        case "copy-formation-info":
+            print("\x1b[93munyohub copy-formation-info\x1b[0m  データベース上で編成データをコピーする")
+            
+            print("\n引数")
+            print("  \x1b[33m第1引数\x1b[0m : コピー元の路線系統識別名")
+            print("  \x1b[33m第2引数\x1b[0m : コピー元の編成名")
+            print("  \x1b[33m第3引数\x1b[0m : コピー先の路線系統識別名")
+            print("  \x1b[33m第4引数\x1b[0m : コピー先の編成名(省略時は第2引数の編成名を使用)")
+        
+        case "clean-formation-db":
+            print("\x1b[93munyohub clean-formation-db\x1b[0m  データベースから編成表にない編成・車両のデータを削除する")
+            
+            print("\n引数")
+            print("  \x1b[33m第1引数\x1b[0m : 路線系統識別名")
+        
+        case "update-operations":
+            print("\x1b[93munyohub update-operations\x1b[0m  運用表をデータベースに書き込む")
+            
+            print("\n引数")
+            print("  \x1b[33m第1引数\x1b[0m : 路線系統識別名")
+            print("  \x1b[33m第2引数\x1b[0m : ダイヤ改正識別名")
+            print("  \x1b[33m第3引数\x1b[0m : ダイヤ識別名")
+            
+            print("\nオプション")
+            print("  \x1b[33m-D\x1b[0m : 運用表を書き込むのではなく削除する")
+        
+        case "user-info":
+            print("\x1b[93munyohub user-info\x1b[0m  ユーザーの情報を表示する")
+            
+            print("\n引数")
+            print("  \x1b[33m第1引数\x1b[0m : ユーザー識別名(省略時はユーザーの総数を表示)")
+        
+        case "create-user":
+            print("\x1b[93munyohub create-user\x1b[0m  ユーザーを新規登録する")
+            
+            print("\n引数")
+            print("  \x1b[33m第1引数\x1b[0m : ユーザー識別名")
+            print("  \x1b[33m第2引数\x1b[0m : ユーザー表示名")
+            print("  \x1b[33m第3引数\x1b[0m : メールアドレス(省略可能)")
+        
+        case "modify-user-info":
+            print("\x1b[93munyohub modify-user-info\x1b[0m  ユーザーの情報を変更する")
+            
+            print("\n引数")
+            print("  \x1b[33m第1引数\x1b[0m : ユーザー識別名")
+            
+            print("\nオプション")
+            print("  \x1b[33m--user-name \"ユーザー表示名\"\x1b[0m : ユーザー表示名を変更する場合は指定")
+            print("  \x1b[33m--email-address \"メールアドレス\"\x1b[0m : メールアドレスを変更する場合は指定")
+            print("  \x1b[33m--website-url \"webサイトのURL\"\x1b[0m : webサイトのURLを変更する場合は指定")
+            print("  \x1b[33m--reset-password\x1b[0m : パスワードを再設定する場合は指定(新しいパスワードはランダムな文字列が自動設定される)")
+            print("  \x1b[33m--time-out 日数\x1b[0m : タイムアウトを設定する場合は指定(日数を0とした場合はタイムアウトが解除される)")
+            print("  \x1b[33m--enable\x1b[0m : アカウントの凍結を解除する場合は指定")
+            print("  \x1b[33m--disable\x1b[0m : アカウントを凍結する場合は指定")
+        
+        case "role-info":
+            print("\x1b[93munyohub role-info\x1b[0m  ロールの情報を表示する")
+            
+            print("\n引数")
+            print("  \x1b[33m第1引数\x1b[0m : ロールID(省略時はロールの一覧を表示)")
+        
+        case "create-role":
+            print("\x1b[93munyohub create-role\x1b[0m  ロールを新規作成する")
+            
+            print("\n引数")
+            print("  \x1b[33m第1引数\x1b[0m : ロールID")
+            print("  \x1b[33m第2引数\x1b[0m : ロール名")
+        
+        case "delete-role":
+            print("\x1b[93munyohub delete-role\x1b[0m  ロールを削除する")
+            
+            print("\n引数")
+            print("  \x1b[33m第1引数\x1b[0m : ロールID")
+        
+        case "add-permission":
+            print("\x1b[93munyohub add-permission\x1b[0m  ロールに権限を割り当てる")
+            
+            print("\n引数")
+            print("  \x1b[33m第1引数\x1b[0m : ロールID")
+            print("  \x1b[33m第2引数\x1b[0m : 権限識別名")
+            print("  \x1b[33m第3引数\x1b[0m : 権限のパラメータ(省略可能)")
+        
+        case "remove-permission":
+            print("\x1b[93munyohub remove-permission\x1b[0m  ロールから権限を剥奪する")
+            
+            print("\n引数")
+            print("  \x1b[33m第1引数\x1b[0m : ロールID")
+            print("  \x1b[33m第2引数\x1b[0m : 権限識別名")
+            print("  \x1b[33m第3引数\x1b[0m : 権限のパラメータ(省略可能)")
+        
+        case "assign-role":
+            print("\x1b[93munyohub assign-role\x1b[0m  ユーザーにロールを割り当てる")
+            
+            print("\n引数")
+            print("  \x1b[33m第1引数\x1b[0m : ユーザーID")
+            print("  \x1b[33m第2引数\x1b[0m : ロールID")
+        
+        case "remove-role":
+            print("\x1b[93munyohub remove-role\x1b[0m  ユーザーからロールを剥奪する")
+            
+            print("\n引数")
+            print("  \x1b[33m第1引数\x1b[0m : ユーザーID")
+            print("  \x1b[33m第2引数\x1b[0m : ロールID")
+        
+        case "init-db":
+            print("\x1b[93munyohub init-db\x1b[0m  各路線系統用データベースファイルを生成する")
+            
+            print("\n引数")
+            print("  \x1b[33m第1引数\x1b[0m : 路線系統識別名")
+        
+        case "init-common-dbs":
+            print("\x1b[93munyohub init-common-dbs\x1b[0m  全路線系統共通データ用データベースファイル群を生成する")
+        
+        case "get-gtfs-realtime":
+            print("\x1b[93munyohub get-gtfs-realtime\x1b[0m  鉄道事業者の公開APIから在線情報を取得し、車両運用情報を更新する")
+            
+            print("\n引数")
+            print("  \x1b[33m第1引数\x1b[0m : 路線系統識別名(省略時はすべての路線系統を処理)")
+            
+            print("\nオプション")
+            print("  \x1b[33m-s\x1b[0m : ログ出力を抑制し、ランダムな待機時間を挿入する")
+        
+        case "update-trip-ids":
+            print("\x1b[93munyohub update-trip-ids\x1b[0m  便識別名と列車番号の対応表をデータベースに書き込む")
+            
+            print("\n引数")
+            print("  \x1b[33m第1引数\x1b[0m : 路線系統識別名")
+            print("  \x1b[33m第2引数\x1b[0m : ダイヤ改正識別名")
+            print("  \x1b[33m第3引数\x1b[0m : ダイヤ識別名")
+            
+            print("\nオプション")
+            print("  \x1b[33m-D\x1b[0m : 対応表を書き込むのではなく削除する")
+        
+        case "generate-operation-table":
+            print("\x1b[93munyohub generate-operation-table\x1b[0m  1日分の在線情報をもとに運用表を生成する")
+            
+            print("\n引数")
+            print("  \x1b[33m第1引数\x1b[0m : 路線系統識別名")
+            print("  \x1b[33m第2引数\x1b[0m : 日付(YYYY-MM-DD)")
+        
+        case _:
+            print("\x1b[31mサブコマンド \x1b[1m" + subcommand + "\x1b[22m は存在しません\x1b[0m")
