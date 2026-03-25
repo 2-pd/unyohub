de819f4b4ba07ff2e85f4b580f77a90f3a9d20dc
kazuma <>
2026-03-25 09:18:18 +0000

    管理画面からユーザーアカウントの停止と有効化を行えるように
    FossilOrigin-Name: 77c2781f29e8c0beb332a98933072ca3d0589c10cfe04f85873445ba51d7c29e

--- unyohub/admin/user_data.php
+++ unyohub/admin/user_data.php
@@ -12,12 +12,15 @@ if (empty($_GET["user_id"])) {
 }
 
 $moderator_id = $user->get_id();
+$moderator_is_admin = $user->check_permission("instance_administrator");
 
 
 print_header();
 
 
 print "<article>";
+
+print "<nav><a href='user_list.php'>ユーザーの一覧</a> &gt;";
 
 print "<h2>ユーザーの詳細</h2>";
 
@@ -33,6 +36,69 @@ if (!is_object($user_obj)) {
 $user_id = $user_obj->get_id();
 
 
+$result_text = NULL;
+
+if (!empty($_POST["enable_user"])) {
+    if (!$user->check_one_time_token($_POST["one_time_token"])) {
+        $result_text = "【!】ワンタイムトークンが無効です。処理はキャンセルされました。";
+        goto on_error;
+    }
+    
+    $user_obj->set_status(WAKARANA_STATUS_NORMAL);
+    $result_text = "ユーザーアカウントを有効化しました";
+} elseif (!empty($_POST["disable_user"])) {
+    if (!$user->check_one_time_token($_POST["one_time_token"])) {
+        $result_text = "【!】ワンタイムトークンが無効です。処理はキャンセルされました。";
+        goto on_error;
+    }
+    
+    if (!$user_obj->check_permission("control_panel_user") || ($moderator_is_admin && $user_id !== $moderator_id)) {
+        $user_obj->set_status(WAKARANA_STATUS_DISABLE);
+        $result_text = "ユーザーアカウントを停止しました";
+    } else {
+        $result_text = "【!】このユーザーのアカウントを停止することはできません。";
+    }
+}
+
+on_error:
+
+if (!empty($result_text)) {
+    print "<script> alert('".$result_text."'); </script>";
+}
+
+
+$user_is_moderator = $user_obj->check_permission("control_panel_user");
+
+
+$token_html = "<input type='hidden' name='one_time_token' value='".$user->create_one_time_token()."'>";
+
+print "<form action='user_data.php?user_id=".$user_id."' method='post' id='enable_form' style='display: none;'>";
+print $token_html;
+print "<input type='hidden' name='enable_user' value='yes'>";
+print "</form>";
+
+print "<form action='user_data.php?user_id=".$user_id."' method='post' id='disable_form' style='display: none;'>";
+print $token_html;
+print "<input type='hidden' name='disable_user' value='yes'>";
+print "</form>";
+
+print <<< EOM
+<script>
+function enable_user (railroad_id, railroad_name) {
+    if (confirm("このユーザーのアカウントを有効化しますか？")) {
+        document.getElementById("enable_form").submit();
+    }
+}
+
+function disable_user (railroad_id, railroad_name) {
+    if (confirm("このユーザーのアカウントを停止しますか？")) {
+        document.getElementById("disable_form").submit();
+    }
+}
+</script>
+EOM;
+
+
 print "<h3>基本情報</h3>";
 
 print "<div class='key_and_value'><b>ユーザー識別名</b>".$user_id."</div>";
@@ -40,7 +106,12 @@ print "<div class='key_and_value'><b>ユーザー識別名</b>".$user_id."</div>
 $user_name = $user_obj->get_name();
 print "<div class='key_and_value'><b>ハンドルネーム</b>".(!empty($user_name) ? htmlspecialchars($user_name) : "(未設定)")."</div>";
 
-print "<div class='key_and_value'><b>状態</b><span style='color: ".($user_obj->get_status() === WAKARANA_STATUS_NORMAL ? "#33cc99;'>有効" : "#ee3333;'>停止中")."</span></div>";
+$user_status = $user_obj->get_status();
+print "<div class='key_and_value'><b>状態</b><span style='color: ".($user_status === WAKARANA_STATUS_NORMAL ? "#33cc99;'>有効" : "#ee3333;'>停止中")."</span>";
+if (!$user_is_moderator || ($moderator_is_admin && $user_id !== $moderator_id)) {
+    print "<div><button type='button' onclick='".($user_status === WAKARANA_STATUS_NORMAL ? "disable_user(\"".$user_id."\");'>アカウントの停止" : "enable_user(\"".$user_id."\");'>アカウントの有効化")."</button></div>";
+}
+print "</div>";
 
 print "<div class='key_and_value'><b>ユーザー登録日時</b>".$user_obj->get_created()."</div>";
 
@@ -81,7 +152,7 @@ $now_datetime = date("Y-m-d H:i:s", $now_ts);
 
 $time_out_expiration = $moderation_db_obj->querySingle("SELECT `expiration_datetime` FROM `unyohub_moderation_timed_out_users` WHERE `user_id` = '".$user_id."' AND `expiration_datetime` > '".$now_datetime."'");
 
-print "<div class='key_and_value'><b>現在の状態</b>".(empty($time_out_expiration) ? "タイムアウトなし" : "<span style='color: #ee7700;'>タイムアウト中 (残 ".(ceil((strtotime($time_out_expiration) - $now_ts) / 86400))."日)</span>")."</div>";
+print "<div class='key_and_value'><b>現在の状態</b>".(empty($time_out_expiration) ? "タイムアウトなし" : "<span style='color: #ee7700;'>タイムアウト中 (残 ".(ceil((strtotime($time_out_expiration) - $now_ts) / 86400))."日)</span>").(!$user_is_moderator ? "<div><button type='button' onclick='location.href = \"/admin/time_out_user.php?user_id=".$user_id."\";'>タイムアウトの設定</button></div>" : "")."</div>";
 
 $logs_r = $moderation_db_obj->query("SELECT `timed_out_datetime`, `moderator_id`, `timed_out_days` FROM `unyohub_moderation_user_timed_out_logs` WHERE `user_id` = '".$user_id."' ORDER BY `timed_out_datetime` DESC LIMIT 5");
 
