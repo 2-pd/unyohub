91af01999c6f313c8dd0520e31752234f7fcd6e2
kazuma <>
2026-03-26 07:00:00 +0000

    管理画面のユーザー詳細ページからメールアドレスのドメインをブラックリストへ追加できるように
    FossilOrigin-Name: 77c5a92385b10ec7997ef85c672a1540a36cf50537343c80bfc52a63204958bf

--- unyohub/admin/email_domain_blacklist.php
+++ unyohub/admin/email_domain_blacklist.php
@@ -1,5 +1,6 @@
 <?php
 include "admin_common.php";
+include "../libs/wakarana/config.php";
 
 if (!$user->check_permission("instance_administrator")) {
     print "【!】アクセス中のユーザーアカウントにはこのページにアクセスする権限がありません";
@@ -12,6 +13,22 @@ print_header();
 
 print "<article>";
 
+
+$wakarana_config = new wakarana_config("../config");
+
+
+if (isset($_POST["additional_email_domain"])) {
+    if (isset($_POST["one_time_token"]) && $user->check_one_time_token($_POST["one_time_token"])) {
+        if ($wakarana_config->add_email_domain_to_blacklist($_POST["additional_email_domain"])) {
+            print "<script> alert('ブロック対象のメールドメインを追加しました'); </script>";
+        } else {
+            print "<script> alert('【!】ブロック対象メールドメインの追加に失敗しました'); </script>";
+        }
+    } else {
+        print "<script> alert('【!】ワンタイムトークンが無効です。処理はキャンセルされました。'); </script>";
+    }
+}
+
 if (isset($_POST["email_domain_blacklist"])) {
     $blacklist = $_POST["email_domain_blacklist"];
     
@@ -23,8 +40,9 @@ if (isset($_POST["email_domain_blacklist"])) {
         print "<script> alert('【!】ワンタイムトークンが無効です。処理はキャンセルされました。'); </script>";
     }
 } else {
-    $blacklist = file_get_contents("../config/wakarana_email_domain_blacklist.conf");
+    $blacklist = implode("\n", $wakarana_config->get_email_domain_blacklist());
 }
+
 
 print "<h2>メールドメインのブロック設定</h2>";
 
