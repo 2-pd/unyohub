6119556f040db48c250109a43c08b58200f4c69b
kazuma <>
2026-04-10 02:00:00 +0000

    APIのoperation_data.phpをデータベーステーブル仕様の変更に対応
    FossilOrigin-Name: 7c55c8756118607af75cc5bc174135dbe5a689c48054d022a1433c3fa42b4f70

--- unyohub/api/operation_data.php
+++ unyohub/api/operation_data.php
@@ -9,46 +9,53 @@ if (!isset($_POST["railroad_id"], $_POST["date"])) {
 $db_obj = new SQLite3("../data/".basename($_POST["railroad_id"])."/railroad.db");
 $db_obj->busyTimeout(5000);
 
-$operations_r = $db_obj->query("SELECT `operation_number`, `formations`, `posts_count`, `variant_exists`, `comment_exists`, `from_beginner`, `is_quotation`, `updated_datetime`, `confirmed_train_final_arrival_time` FROM `unyohub_data_caches` WHERE `operation_date` = '".$db_obj->escapeString($_POST["date"])."'".(!empty($_POST["last_modified_timestamp"]) ? " AND `updated_datetime` > '".date("Y-m-d H:i:s", intval($_POST["last_modified_timestamp"]))."'" : "")." ORDER BY `updated_datetime` DESC, `operation_number` DESC, `assign_order` DESC");
+$assigned_formations_r = $db_obj->query("SELECT `operation_number`, `formations` FROM `unyohub_assigned_formation_caches` WHERE `operation_date` = '".$db_obj->escapeString($_POST["date"])."'".(!empty($_POST["last_modified_timestamp"]) ? " AND `updated_datetime` > '".date("Y-m-d H:i:s", intval($_POST["last_modified_timestamp"]))."'" : "")." ORDER BY `updated_datetime` DESC, `operation_number` DESC, `assign_order` DESC");
+$metadata_r = $db_obj->query("SELECT `operation_number`, `posts_count`, `variant_exists`, `comment_exists`, `from_beginner`, `is_quotation`, `updated_datetime`, `confirmed_train_final_arrival_time` FROM `unyohub_metadata_caches` WHERE `operation_date` = '".$db_obj->escapeString($_POST["date"])."'".(!empty($_POST["last_modified_timestamp"]) ? " AND `updated_datetime` > '".date("Y-m-d H:i:s", intval($_POST["last_modified_timestamp"]))."'" : "")." ORDER BY `updated_datetime` DESC, `operation_number` DESC");
 
 $operation_data = array();
-while ($operation = $operations_r->fetchArray(SQLITE3_ASSOC)) {
+$assigned_formation_data = $assigned_formations_r->fetchArray(SQLITE3_ASSOC);
+while ($metadata = $metadata_r->fetchArray(SQLITE3_ASSOC)) {
     if (empty($operation_data)) {
-        $updated_datetime = $operation["updated_datetime"];
-    } elseif (isset($operation_data[$operation["operation_number"]])) {
-        if (!isset($operation_data[$operation["operation_number"]]["relieved_formations"])) {
-            $operation_data[$operation["operation_number"]]["relieved_formations"] = array($operation["formations"]);
-        } else {
-            array_unshift($operation_data[$operation["operation_number"]]["relieved_formations"], $operation["formations"]);
-        }
-        
+        $updated_datetime = $metadata["updated_datetime"];
+    }
+    
+    if (empty($assigned_formation_data) || $assigned_formation_data["operation_number"] !== $metadata["operation_number"]) {
+        $operation_data[$metadata["operation_number"]] = NULL;
         continue;
     }
     
-    if (!is_null($operation["formations"])) {
-        $operation_data_item = array("formations" => $operation["formations"], "posts_count" => $operation["posts_count"], "confirmed_train_final_arrival_time" => $operation["confirmed_train_final_arrival_time"]);
-        
-        if ($operation["variant_exists"]) {
-            $operation_data_item["variant_exists"] = TRUE;
-        }
-        if ($operation["comment_exists"]) {
-            $operation_data_item["comment_exists"] = TRUE;
-        }
-        if ($operation["from_beginner"]) {
-            $operation_data_item["from_beginner"] = TRUE;
-        }
-        if ($operation["is_quotation"]) {
-            $operation_data_item["is_quotation"] = TRUE;
-        }
-        
-        if (!empty($_POST["require_last_posted_datetime"])) {
-            $operation_data_item["last_posted_datetime"] = $operation["updated_datetime"];
-        }
-        
-        $operation_data[$operation["operation_number"]] = $operation_data_item;
-    } else {
-        $operation_data[$operation["operation_number"]] = NULL;
+    $operation_data_item = array("formations" => is_null($assigned_formation_data["formations"]) ? "" : $assigned_formation_data["formations"], "posts_count" => $metadata["posts_count"], "confirmed_train_final_arrival_time" => $metadata["confirmed_train_final_arrival_time"]);
+    
+    if ($metadata["variant_exists"]) {
+        $operation_data_item["variant_exists"] = TRUE;
     }
+    if ($metadata["comment_exists"]) {
+        $operation_data_item["comment_exists"] = TRUE;
+    }
+    if ($metadata["from_beginner"]) {
+        $operation_data_item["from_beginner"] = TRUE;
+    }
+    if ($metadata["is_quotation"]) {
+        $operation_data_item["is_quotation"] = TRUE;
+    }
+    
+    if (!empty($_POST["require_last_posted_datetime"])) {
+        $operation_data_item["last_posted_datetime"] = $metadata["updated_datetime"];
+    }
+    
+    while ($assigned_formation_data = $assigned_formations_r->fetchArray(SQLITE3_ASSOC)) {
+        if ($assigned_formation_data["operation_number"] !== $metadata["operation_number"]) {
+            break;
+        }
+        
+        if (!isset($operation_data_item["relieved_formations"])) {
+            $operation_data_item["relieved_formations"] = array(is_null($assigned_formation_data["formations"]) ? "" : $assigned_formation_data["formations"]);
+        } else {
+            array_unshift($operation_data_item["relieved_formations"], is_null($assigned_formation_data["formations"]) ? "" : $assigned_formation_data["formations"]);
+        }
+    }
+    
+    $operation_data[$metadata["operation_number"]] = $operation_data_item;
 }
 
 if (empty($operation_data)) {
