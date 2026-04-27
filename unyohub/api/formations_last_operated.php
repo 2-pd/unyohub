d60ae83e2deb301fff74f01ffa63e71b04d00281
kazuma <>
2026-04-27 08:00:00 +0000

    APIエンドポイント formations_last_operated.php を実装
    FossilOrigin-Name: d47f0090442b0a8ba98a3ae7db3c67601df1260258e1c04050119891c4006f39

---
+++ unyohub/api/formations_last_operated.php
@@ -0,0 +1,56 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+
+if (!isset($_POST["railroad_id"])) {
+    print "ERROR: 送信値が不正です";
+    exit;
+}
+
+$db_obj = new SQLite3("../data/".basename($_POST["railroad_id"])."/railroad.db");
+$db_obj->busyTimeout(5000);
+
+$sql_r = $db_obj->query("
+    SELECT
+        `unyohub_data_each_formation`.`formation_name`,
+        `unyohub_data_each_formation`.`operation_date`,
+        `unyohub_assigned_formation_caches`.`assign_order`,
+        `unyohub_assigned_formation_caches`.`formations`
+    FROM `unyohub_data_each_formation`
+    JOIN `unyohub_formations` ON `unyohub_data_each_formation`.`formation_name` = `unyohub_formations`.`formation_name`
+    JOIN `unyohub_assigned_formation_caches` ON `unyohub_data_each_formation`.`operation_date` = `unyohub_assigned_formation_caches`.`operation_date` AND `unyohub_data_each_formation`.`operation_number` = `unyohub_assigned_formation_caches`.`operation_number`
+    JOIN (
+        SELECT `formation_name`, MAX(`operation_date`) AS `max_date`
+        FROM `unyohub_data_each_formation`
+        WHERE `operation_date` <= '".date("Y-m-d", time() - 14400)."'
+        GROUP BY `formation_name`
+    ) AS `latest` ON `unyohub_data_each_formation`.`formation_name` = `latest`.`formation_name` AND `unyohub_data_each_formation`.`operation_date` = `latest`.`max_date`
+    WHERE `unyohub_formations`.`currently_registered` = 1
+    ORDER BY `unyohub_data_each_formation`.`formation_name` ASC, `unyohub_assigned_formation_caches`.`assign_order` DESC
+");
+
+$operation_data = array();
+while ($row = $sql_r->fetchArray(SQLITE3_ASSOC)) {
+    $formation_name = $row["formation_name"];
+    
+    if (!isset($operation_data[$formation_name])) {
+        $operation_data[$formation_name] = array("operation_date" => $row["operation_date"], "formations" => is_null($row["formations"]) ? "" : $row["formations"]);
+    } else {
+        if (!isset($operation_data[$formation_name]["relieved_formations"])) {
+            $operation_data[$formation_name]["relieved_formations"] = array();
+        }
+        
+        array_unshift($operation_data[$formation_name]["relieved_formations"], is_null($row["formations"]) ? "" : $row["formations"]);
+    }
+}
+
+if (empty($operation_data)) {
+    print "NO_UPDATES_AVAILABLE";
+} else {
+    if (!empty($_SERVER["HTTP_ACCEPT_ENCODING"]) && str_contains($_SERVER["HTTP_ACCEPT_ENCODING"], "gzip")) {
+        header("Content-Encoding: gzip");
+        
+        print gzencode(json_encode($operation_data, JSON_UNESCAPED_UNICODE));
+    } else {
+        print json_encode($operation_data, JSON_UNESCAPED_UNICODE);
+    }
+}
